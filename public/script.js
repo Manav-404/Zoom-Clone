573fab44675fd978307378bd1d3acd82fff38628
Manav <manavdesai30399@gmail.com>
2020-08-10 10:03:08 +0530

    resolved peer issue

--- public/script.js
+++ public/script.js
@@ -1,47 +1,44 @@
-
 let vidStream;
-var peer = new Peer(undefined , {
-    path : '/peerjs' , 
-    host : '/' , 
-    port : '3030',
-
+var peer = new Peer(undefined, {
+	path: "/peerjs",
+	host: "/",
+	port: "3030",
 });
-const socket = io('/')
+const socket = io("/");
 const vidGrid = document.getElementById("video-grid");
-const vid = document.createElement('video');
+const vid = document.createElement("video");
 vid.muted = true;
 
+navigator.mediaDevices
+	.getUserMedia({
+		video: true,
+		audio: true,
+	})
+	.then((stream) => {
+		vidStream = stream;
+		addVidStream(vid, vidStream);
+	})
+	.catch((err) => console.log(err));
 
-
-navigator.mediaDevices.getUserMedia({
-    video: true,
-    audio  :true 
-}).then(stream =>{
-    vidStream = stream;
-    addVidStream(vid , vidStream);
-
-})
-.catch(err=>console.log(err));
-
-peer.on('open' , id =>{ 
-    console.log(id)
+peer.on("open", (id) => {
+	socket.emit("join-room", ROOM_ID, id);
 });
 
-socket.emit('join-room' , ROOM_ID);
-socket.on('user-connected' , ()=>{
-    connectToNewUser();
+socket.emit("join-room", ROOM_ID);
+
+socket.on("user-connected", (userId) => {
+	connectToNewUser(userId);
 });
 
-const connectToNewUser = () => {
-    console.log("new user");
-}
+const connectToNewUser = (userId) => {
+	console.log(userId);
+};
 
-const addVidStream = (video , stream) =>{
-    video.srcObject = stream;
-    video.addEventListener('loadedmetadata' , ()=>{
-        video.play();
-    })
+const addVidStream = (video, stream) => {
+	video.srcObject = stream;
+	video.addEventListener("loadedmetadata", () => {
+		video.play();
+	});
 
-    vidGrid.append(video);
-}
-
+	vidGrid.append(video);
+};
